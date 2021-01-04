b252483120a356e5f2b1b1dbb26e07936a4ca23f
Felipe F. Rocha <felipefonsecarocha@gmail.com>
2021-01-03 21:54:52 -0300

    Modelo - Van Der Pol + Runge Kutta

--- runge_kutta_van_der_pol.py
+++ runge_kutta_van_der_pol.py
@@ -3,7 +3,7 @@ import random
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-
+import math
 mpl.rcParams['legend.fontsize'] = 10
 
 
@@ -11,15 +11,18 @@ mpl.rcParams['legend.fontsize'] = 10
 
 
 def dvCord(x, ux, uy, t):
-    a = 0.258
-    b = 4.033
-    F = 8
-    G = 1
+    # a = 0.258
+    # b = 4.033
+    # F = 8
+    # G = 1
+    epsilon = 8.53
+
+
     xd = np.array(np.zeros((3, 1)))
 
-    xd[0] = -x[1] - x[2] - a * x[1] + a * F
-    xd[1] = x[0] * x[1] - b * x[0] * x[2] - x[1] + G
-    xd[2] = b * x[0] * x[1] + x[0] * x[2] - x[2]
+    xd[0] = x[1]
+    xd[1] = -x[0] + epsilon * x[1] + ux - epsilon * x[0]**2 * x[1]
+    # xd[2] = x[0]
 
     return (xd.copy())
 
@@ -55,25 +58,28 @@ def rkCord(x0, ux, uy, h, t):
 
 
 if __name__ == "__main__":
-    t0 = 0
-    tf = 150
+    t0 = 900
+    tf = 1000
     h = 0.01
     t = np.arange(t0, tf, h)
     [print(i) for i in t if i < 0.1]
 
-    x0 = np.array([[0.1], [0.1], [0.1]])
+    x0 = np.array([[0.1], [0.1]])
 
     z_x = np.zeros((len(x0), len(t) - 1))
     x = x0.copy()
     x = np.append(x, z_x, axis=1)
     print(x)
-    u = np.zeros((len(t), 1))
-
+    Ts = 1/100
+    omega = (2 * math.pi) / 10
+    U = 1.2
+    u = np.array([U * math.sin(omega * i * Ts) for i in t])  #np.zeros((len(t), 1))
+    
     for k in range(1, len(t)):
         result = rkCord(x[:, k - 1].copy(), u[k], u[k], h, t[k])
         x[:, k] = result
 
-    fig, ax = plt.subplots(constrained_layout=True, figsize=(4, 8))
+    fig, ax = plt.subplots(constrained_layout=True, figsize=(8, 4))
     ax.set(xlabel='time')
     ax.set(ylabel='x_1')
     color = ['red', 'green', 'blue', 'yellow', 'orange']
@@ -86,8 +92,9 @@ if __name__ == "__main__":
     ax1.plot(t, x[1, :], color=color[random.randint(0, len(color) - 1)])
     ax1.grid()
     fig1.savefig(f'x_2.png')
-    fig2 = plt.figure(constrained_layout=True, figsize=(8, 8))
-    ax2 = fig2.gca(projection='3d')
-    ax2.plot(x[0, 1000:], x[1, 1000:], x[2, 1000:], color=color[random.randint(0, len(color) - 1)])
-    ax2.grid()
-    fig2.savefig(f'x_3.png')
+    plt.show()
+    # fig2 = plt.figure(constrained_layout=True, figsize=(8, 8))
+    # ax2 = fig2.gca(projection='3d')
+    # ax2.plot(x[0, 1000:], x[1, 1000:], x[2, 1000:], color=color[random.randint(0, len(color) - 1)])
+    # ax2.grid()
+    # fig2.savefig(f'x_3.png')
